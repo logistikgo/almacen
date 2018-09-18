c12db9beb5828784aa1fda8c0d1d43e392ed470d
AlexMethod <alvalex_@hotmail.com>
2018-09-18 13:05:30 -0500

    Se agregan los apis para usuarios

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const Producto = require('./controllers/Producto');
+const Usuario = require('./controllers/Usuario');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.get('/api/productos', Producto.get);
 app.get('/api/productos/:idClienteFiscal',Producto.getByIDClienteFiscal);
+app.get('/api/usuarios',Usuario.get);
+app.get('/api/usuarios/:idusuario',Usuario.getByIDUsuario);
 
 module.exports = app;
