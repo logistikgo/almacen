e5800f5103ea2640b215981398a623a917e9d3d5
AlexMethod <alvalex_@hotmail.com>
2018-09-18 16:56:00 -0500

    Validacion helpers

--- helpers.js
+++ helpers.js
@@ -1,8 +1,11 @@
 async function getNextID(dataContext,field){
 	let max = 0;
 	let lastUser = await dataContext.find().sort([[field,-1]]).exec();
-	max = (lastUser[0])[field] + 1;
-	return max;
+	
+	if(lastUser[0]!=null)
+		max = (lastUser[0])[field];
+
+	return max + 1;
 }
 
 module.exports = {
