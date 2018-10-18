986809c97b963d3a5bd3c3b3fc22aeab744d8b35
AlexMethod <alvalex_@hotmail.com>
2018-10-18 12:38:46 -0500

    Cambio entorno a produccion 2

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
 	//db: process.env.MONGODB || 'mongodb://admin:R9VdEsuAXRY3@ds018268.mlab.com:18268/logistikgo_test',//TEST 3
 	//DEMO
 	//db: process.env.MONGODB || 'mongodb://admin:R9VdEsuAXRY3@ds121163.mlab.com:21163/alm_demo',//DEMO
-	PRODUCCION
-	//db: process.env.MONGODB || 'mongodb://admin:R9VdEsuAXRY3@ds127843.mlab.com:27843/alm',//PRODUCCION
+	//PRODUCCION
+	db: process.env.MONGODB || 'mongodb://admin:R9VdEsuAXRY3@ds127843.mlab.com:27843/alm',//PRODUCCION
 }
