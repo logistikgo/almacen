ed1e218179b5c9bba73e8fed327a6cd2208cdffe
zazilibarra <zaziil.97@gmail.com>
2019-10-19 12:04:17 -0500

    Columnas x Operacion

--- models/ColumnasxTipoUsuario.js
+++ models/ColumnasxTipoUsuario.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ColumnasxTipoUsuario = Schema({
-	tipoUsuario : String,
-    idTabla : String,
+    tipoUsuario: String,
+    idTabla: String,
     columnas: [
-    {
-    	indice: Number,
-        nombre: String,
-        enumeracion: String
-    }],
+        {
+            indice: Number,
+            nombre: String,
+            enumeracion: String
+        }],
 },
-{ collection: 'ColumnasxTipoUsuario' });
+    { collection: 'ColumnasxTipoUsuario' });
 
 module.exports = mongoose.model('ColumnasxTipoUsuario', ColumnasxTipoUsuario);
