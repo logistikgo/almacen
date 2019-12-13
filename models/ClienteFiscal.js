64bf2b4e58e40f689ccb11d40c270fcead6e9714
zazilibarra <zaziil.97@gmail.com>
2019-12-13 17:26:20 -0600

    Tipo de tarifa en alta de cliente

--- models/ClienteFiscal.js
+++ models/ClienteFiscal.js
@@ -4,31 +4,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ClienteFiscal = Schema({
-	idCliente:Number,
-	usuarioAlta_id : Number,
-	usuarioEdita_id :Number,
-	nombreUsuario : String,
-	idSucursal : Number,
-	sucursal_id:{type:Schema.ObjectId,ref:'Sucursal'},
-	fechaAlta:Date,
-	fechaElimina:Date,
-	fechaEdita:Date,
-	nombreCorto:String,
-	nombreComercial:String,
-	razonSocial:String,
+	idCliente: Number,
+	usuarioAlta_id: Number,
+	usuarioEdita_id: Number,
+	nombreUsuario: String,
+	idSucursal: Number,
+	sucursal_id: { type: Schema.ObjectId, ref: 'Sucursal' },
+	fechaAlta: Date,
+	fechaElimina: Date,
+	fechaEdita: Date,
+	nombreCorto: String,
+	nombreComercial: String,
+	razonSocial: String,
 	clave: String,
-	rfc : String,
-	calle:String,
-	numExt:String,
-	numInt:String,
-	cp:Number,
-	colonia:String,
-	municipio:String,
-	estado:String,
-	pais:String,	
-	statusReg:String	
+	rfc: String,
+	calle: String,
+	numExt: String,
+	numInt: String,
+	cp: Number,
+	colonia: String,
+	municipio: String,
+	estado: String,
+	pais: String,
+	tipoTarifaPrecio: String,
+	statusReg: String
 },
-{collection:'ClientesFiscales'}
+	{ collection: 'ClientesFiscales' }
 );
 
-module.exports = mongoose.model('ClienteFiscal',ClienteFiscal);
+module.exports = mongoose.model('ClienteFiscal', ClienteFiscal);
