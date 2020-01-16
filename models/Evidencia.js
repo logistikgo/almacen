d30789ec8fd18ce397f4c2b46d1a2b068a43cbb4
Dennise Ibarra <dennib.207@gmail.com>
2020-01-16 15:51:59 -0600

    Campo de folio evidencia agregado al modelo

--- models/Evidencia.js
+++ models/Evidencia.js
@@ -9,6 +9,7 @@ const Evidencia = Schema(
 		rutaArchivo:String,
 		tipo:String,
 		tipoTarifa:String,
+		idEvidencia: String,
 		tarifa_id:{type:Schema.ObjectId},
 		entrada_id:{type:Schema.ObjectId,ref:'Entrada'},
 		salida_id:{type:Schema.ObjectId,ref:'Salida'},
