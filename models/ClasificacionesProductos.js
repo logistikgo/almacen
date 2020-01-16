6a84664621f7f7450f4fc18d6addcf14e86e8437
zazilibarra <zaziil.97@gmail.com>
2020-01-16 12:24:48 -0600

    Modelo de Clasificaciones de Producto

---
+++ models/ClasificacionesProductos.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ClasificacionesProductos = Schema(
+    {
+        clave: String,
+        nombre: String,
+        subclasificacion: [{
+            clave: String,
+            nombre: String,
+        }],
+        usuarioAlta: String,
+        usuarioAlta_id: Number,
+        fechaAlta: { type: Date, default: Date.now },
+        usuarioBaja_id: Number,
+        fechaBaja: Date,
+        statusReg: { type: String, default: "ACTIVO" }
+    },
+    {
+        collection: "ClasificacionesProductos"
+    });
+
+module.exports = mongoose.model("ClasificacionesProductos", ClasificacionesProductos);
