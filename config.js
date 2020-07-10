2fa09ad37d8bcc3face09becfba0e9c6a4a40d97
Victor Yael Ruiz Alvarado <vyruiz@logisti-k.com.mx>
2020-07-09 22:23:02 -0500

    config

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ dotenv.config();
 module.exports = {
 	port: process.env.PORT || 8080,
 	//TEST
-	//db: process.env.MONGODB || process.env.DB_TEST_URL_ACCESS,//TEST 3
+	db: process.env.MONGODB || process.env.DB_TEST_URL_ACCESS,//TEST 3
 	//DEMO
-	db: process.env.MONGODB || process.env.DB_DEMO_URL_ACCESS,//DEMO
+	//db: process.env.MONGODB || process.env.DB_DEMO_URL_ACCESS,//DEMO
 	//PRODUCCION
 	//db: process.env.MONGODB || process.env.DB_PRODUCTION_URL_ACCESS,//PRODUCCION
 }
