463a11d1fb6893d9bebbd7caa796c4e9a8204b64
Eliazar Lopez Robles <eliazardev@gmail.com>
2020-12-15 13:20:19 -0600

    Prueba reporte caducidades con 500 registros

--- controllers/Entrada.js
+++ controllers/Entrada.js
@@ -1244,7 +1244,8 @@ async function getExcelCaducidades(req, res) {
 	console.log(filter);
 	PartidaModel.aggregate([{$lookup: {from: "Entradas", localField: "entrada_id", foreignField: "_id", as: "entrada_id"}},
         					   {$lookup: {from: "Productos", localField: "producto_id", foreignField: "_id", as: "producto_id"}},
-        						{$match: filter}
+								{$match: filter},
+								{$limit: 500}
 
 		]).then (async (partida)=> {
 		//console.log("test");
