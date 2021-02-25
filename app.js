957d95c252913d10e52fc24e8657fa07b4d44d16
Eliazar Lopez Robles <eliazardev@gmail.com>
2021-02-25 09:25:11 -0600

    Se agrego endpoint para verificar si un pedido tiene partidas que ya salieron

--- app.js
+++ app.js
@@ -300,6 +300,7 @@ app.post('/api/saveSalidaBabel', Salida.saveSalidaBabel);
 app.post('/api/removefromSalidaId', Salida.removefromSalidaId);
 app.post('/api/agregarPartidaSalidaId', Salida.agregarPartidaSalidaId);
 app.post('/api/SalidaAuto', Salida.saveDashboard);
+app.get('/api/verificarPartidasSalidas/:salida_id', Partida.verificarPartidasSalidas);
 
 //PosicionamentoAuto
 app.post('/api/posicionarPrioridades', Entrada.posicionarPrioridades);
