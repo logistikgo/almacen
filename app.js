e39b92fc496397dd49eb2a15f37ce68aa8e208b5
Eliazar Lopez Robles <eliazardev@gmail.com>
2021-04-05 12:28:46 -0500

    Configuracion del endpoint, para eliminar las partidas, que sus embalajes estan en cero.

--- app.js
+++ app.js
@@ -195,6 +195,7 @@ app.put('/api/partida/updateCajasPedidas', Partida.updateCajasPedidas);
 app.post('/api/posicionarPartidas', Partida.posicionarPartidas);
 app.get('/api/getPartidaMod', Partida.getPartidaMod);
 app.post('/api/LimpiaPosicion', Partida.LimpiaPosicion);
+app.post('/api/removerPartidasEnCero', Partida.removePartidasWithZeroQuantity);
 //app.get('/api/partida', Entrada.getPartidaById);
 //app.put('/api/partida', Entrada.updatePartida);
 //app.get('/api/partidas/:producto_id/:embalaje/:cantidad',Partida.getByProductoEmbalaje);
